51ec8fd96af00ee9b252d44f8b5fb5bf64e270c7
OrxanMustafayev <68545038+Orkhan93@users.noreply.github.com>
2023-11-10 14:20:17 +0400

    initial commit

---
+++ src/main/java/az/spring/notehub/NotehubApplication.java
@@ -0,0 +1,13 @@
+package az.spring.notehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/spring/notehub/NotehubApplicationTests.java
@@ -0,0 +1,13 @@
+package az.spring.notehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
