f492ab8cc5b0a7a85b73c2d5181eae43e37d362b
OrxanMustafayev <68545038+Orkhan93@users.noreply.github.com>
2023-11-19 01:47:49 +0400

    Add custom annotations in request layers

--- src/main/java/az/spring/notehub/request/UserRequest.java
+++ src/main/java/az/spring/notehub/request/UserRequest.java
@@ -1,6 +1,10 @@
 package az.spring.notehub.request;
 
+import az.spring.notehub.constraint.validation.Name;
+import az.spring.notehub.constraint.validation.Password;
+import az.spring.notehub.constraint.validation.Username;
 import az.spring.notehub.enums.UserRole;
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +12,16 @@ import lombok.Setter;
 @Getter
 public class UserRequest {
 
+    @Name
     private String name;
+
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?``{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
+
+    @Username
     private String username;
+
+    @Password
     private String password;
     private UserRole userRole;
 
